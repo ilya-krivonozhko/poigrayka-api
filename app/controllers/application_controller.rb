7e485780a1f89b4597e91184c3714fdadf7c3edf
Ilya Krivonozhko <ilya.krivonozhko@gmail.com>
2026-01-21 12:37:58 +0000

    Add global error handling for RecordNotFound and RecordInvalid in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do
+    head :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { errors: e.record.errors }, status: :unprocessable_content
+  end
 end
